cdc2a70208572f101a7966636fbe9fd65e8d28e8
Oigbochie Blessed <eprince1800@gmail.com>
2024-03-01 18:19:48 +0100

    task 2: Select some Amenities to be comfortable!

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  let amenities = {};
+
+  $('input[type="checkbox"]').change(function() {
+    let amenityId = $(this).attr('data-id');
+    let amenityName = $(this).attr('data-name');
+
+    if ($(this).is(':checked')) {
+      amenities[amenityId] = amenityName;
+    } else {
+      delete amenities[amenityId];
+    }
+
+    var amenityList = Object.values(amenities).join(', ');
+    $('#amenities').text(amenityList);
+  });
+})
