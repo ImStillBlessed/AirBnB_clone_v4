1baca974b5c634d09a210f264d38a4926aea196d
Oigbochie Blessed <eprince1800@gmail.com>
2024-03-04 11:00:04 +0100

    ammended the jquery 2-hbnb

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,12 +1,18 @@
-const request = require('request')
 $(document).ready(function() {
   let amenities = {};
 
-  if (request("http://0.0.0.0:5001/api/v1/status/") === 200) {
-    $('div#api_status').addClass("available")
-  } else {
-    $('div#api_status').removeClass("available")
-  }
+  fetch("http://0.0.0.0:5001/api/v1/status/")
+    .then(response => {
+      if (response.ok) {
+        $('div#api_status').addClass("available")
+      } else {
+        $('div#api_status').removeClass("available")
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      $('div#api_status').removeClass("available")
+    });
 
   $('input[type="checkbox"]').change(function() {
     let amenityId = $(this).attr('data-id');
